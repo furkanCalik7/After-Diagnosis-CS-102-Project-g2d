b0afeedd44fb057fcbff6b7ea3fc026198d49a53
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2020-12-13 17:19:46 +0300

    User class is updated

--- User.java
+++ User.java
@@ -1,18 +1,73 @@
-public class User {
+public abstract class User {
 
     String username;
     String password;
     String email;
     String name;
     String surname;
+    String sex;
+    String userType;
 
 
-    public User(String username, String password, String email, String name, String surname) {
+    public User(String username, String userType, String password, String email, String name, String surname, String sex) {
         this.username = username;
         this.password = password;
         this.email  = email;
         this.name = name;
         this.surname = surname;
+        this.sex = sex;
+        this.userType = userType;
     }
 
+    // +isOnline(): boolean
+    public void setUserName(String uname) {
+        username = uname;
+    }
+
+    public void setPassword(String pass) {
+        password = pass;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
