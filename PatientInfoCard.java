c62efb8160d3e5121180974c6f822c62e1b9a557
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2020-12-17 17:20:59 +0300

    PatientInfoCard is implemented

--- PatientInfoCard.java
+++ PatientInfoCard.java
@@ -1,69 +1,32 @@
-import java.sql.Date;
 
-public class PatientInfoCard {
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class PatientInfoCard extends User {
 
     private int age;
     private Date dob;
     private String bloodType;
     private String allergies;
     private String surgeries;
+    private MySQLAccess access;
+    private ArrayList<Drug> drugs;
 
-    public PatientInfoCard(int age, Date dob, String bloodType, String allergies, String surgeries) {
-        this.age = age;
-        this.dob = dob;
-        this.bloodType = bloodType;
-        this.allergies = allergies;
-        this.surgeries = surgeries;
+    public PatientInfoCard(String username, String userType, String password, String email, String name, String surname, String sex) {
+        super(username, userType, password, email, name, surname, sex);
+        drugs = access.getDrugs(getUserName());
+        updatePatientInfo();
     }
 
-    public String getSurgeries() {
-        return surgeries;
-    }
-
-    public String getBloodType() {
-        return bloodType;
-    }
-
-    public String getAllergies() {
-        return allergies;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setAllergies(String allergies) {
-        this.allergies = allergies;
-    }
-
-    public void setBloodType(String bloodType) {
-        this.bloodType = bloodType;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    public void setSurgeries(String surgeries) {
-        this.surgeries = surgeries;
-    }
-
-    @Override
-    public String toString() {
-        return "PatientInfoCard{" +
-                "age=" + age +
-                ", dob=" + dob +
-                ", bloodType='" + bloodType + '\'' +
-                ", allergies='" + allergies + '\'' +
-                ", surgeries='" + surgeries + '\'' +
-                '}';
+    public void updatePatientInfo() {
+        access = new MySQLAccess();
+        ArrayList<Object> dataList = access.getPatientInfo(getUserName());
+        if (dataList.size() > 0) {
+            dob = (Date) dataList.get(0);
+            bloodType = (String) dataList.get(1);
+            age = (Integer) dataList.get(2);
+            allergies = (String) dataList.get(3);
+            surgeries = (String) dataList.get(4);
+        }
     }
 }
