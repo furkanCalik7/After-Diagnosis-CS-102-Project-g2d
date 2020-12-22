54719aad7d4e483c0c9323c40ecd9476366e869d
Berkay Calmaz <berkay.calmaz@ug.bilkent.edu.tr>
2020-12-23 00:34:28 +0300

    Added button color changes to the LabTestsMainPanel.

--- src/doctorViews/DoctorMainFrameViewer.java
+++ src/doctorViews/DoctorMainFrameViewer.java
@@ -36,7 +36,7 @@ public class DoctorMainFrameViewer extends JFrame {
     private JPanel doctorHomepagePanelView;
     private MyPatientsLayeredPanelView myPatientsLayeredPanelView;
     private Doctor doctor;
-
+    private LabTestsMainPanel labTestsMainPanel;
 
     /**
      * Internal method to change panels.
@@ -84,7 +84,7 @@ public class DoctorMainFrameViewer extends JFrame {
         contentPane.add(buttonPanel, BorderLayout.WEST);
 
         doctorHomepagePanelView = new DoctorHomepagePanelView( doctor );
-
+        labTestsMainPanel = new LabTestsMainPanel();
         myPatientsLayeredPanelView = new MyPatientsLayeredPanelView(doctor);
 
         JPanel layeredPanePanel = new JPanel();
@@ -98,6 +98,7 @@ public class DoctorMainFrameViewer extends JFrame {
         //Panels need to be added in inverse order
         layeredPane.add(myPatientsLayeredPanelView);
         layeredPane.add(doctorHomepagePanelView);
+        layeredPane.add(labTestsMainPanel);
 
         //Button initializings
         JButton homepageButton = new JButton("Homepage");
@@ -143,6 +144,7 @@ public class DoctorMainFrameViewer extends JFrame {
         JButton labTestsButton = new JButton("Lab Tests");
         labTestsButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                switchPanels(labTestsMainPanel);
             }
         });
         buttonPanel.add( labTestsButton );
