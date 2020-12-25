186c845ae6521425f12f1601f12aad57a9b000bb
emre turhan <emre.turhan@ug.bilkent.edu.tr>
2020-12-25 23:36:29 +0300

    Worker main panel updated

--- src/AdminViews/AddWorkerMainPanel.java
+++ src/AdminViews/AddWorkerMainPanel.java
@@ -4,12 +4,8 @@ import Admin.model.Admin;
 
 
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
+import java.awt.*;
 import javax.swing.border.EmptyBorder;
-import java.awt.Panel;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -24,68 +20,71 @@ public class AddWorkerMainPanel extends JPanel {
     JRadioButton maleButton;
     JRadioButton femaleButton;
     Admin admin;
+    ButtonGroup genderButtonGroup = new ButtonGroup();
 
 
     /**
      * Create the panel.
      */
     public AddWorkerMainPanel(Admin admin) {
-        setBackground(Color.MAGENTA);
+        setBackground(new Color(30, 144, 255));
         setBorder(new EmptyBorder(30, 30, 30, 30));
         setLayout(new BorderLayout(0, 0));
+
         JLabel lblNewLabel = new JLabel("ADD A HOSPITAL WORKER");
+        lblNewLabel.setFont(new Font("Century", Font.BOLD, 20));
         lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(lblNewLabel, BorderLayout.NORTH);
 
         JPanel panel = new JPanel();
-        panel.setBackground(Color.MAGENTA);
+        panel.setBackground(new Color(30, 144, 255));
         add(panel, BorderLayout.WEST);
 
         JPanel panel_1 = new JPanel();
-        panel_1.setBackground(Color.MAGENTA);
+        panel_1.setBackground(new Color(30, 144, 255));
         add(panel_1, BorderLayout.SOUTH);
 
         JPanel panel_2 = new JPanel();
-        panel_2.setBackground(Color.MAGENTA);
+        panel_2.setBackground(new Color(30, 144, 255));
         add(panel_2, BorderLayout.EAST);
 
         JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(new Color(30, 144, 255));
         add(centerPanel, BorderLayout.CENTER);
-        centerPanel.setLayout(new GridLayout(7, 0, 0, 0));
+        centerPanel.setLayout(new GridLayout(8, 0, 5, 10));
 
         JPanel headerPanel = new JPanel();
-        headerPanel.setBackground(Color.MAGENTA);
+        headerPanel.setBackground(new Color(30, 144, 255));
         centerPanel.add(headerPanel);
         headerPanel.setLayout(new BorderLayout(0, 0));
 
         JLabel lblNewLabel_1 = new JLabel("Select A Hospital Worker Type");
-        lblNewLabel_1.setBackground(Color.MAGENTA);
+        lblNewLabel_1.setFont(new Font("Century", Font.BOLD, 15));
+        lblNewLabel_1.setBackground(new Color(30, 144, 255));
         lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
         headerPanel.add(lblNewLabel_1);
 
         JPanel radioButtonPanel = new JPanel();
-        radioButtonPanel.setBackground(Color.MAGENTA);
+        radioButtonPanel.setBackground(new Color(30, 144, 255));
         centerPanel.add(radioButtonPanel);
         radioButtonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
-        doctorButton = new JRadioButton("Doctor");
+        JRadioButton doctorButton = new JRadioButton("Doctor");
+        doctorButton.setFont(new Font("Century", Font.PLAIN, 15));
         doctorButton.setHorizontalAlignment(SwingConstants.LEFT);
         radioButtonPanel.add(doctorButton);
 
-        labTechButton = new JRadioButton("Lab Technician");
+        JRadioButton labTechButton = new JRadioButton("Lab Technician");
+        labTechButton.setFont(new Font("Century", Font.PLAIN, 15));
         radioButtonPanel.add(labTechButton);
 
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(labTechButton);
-        bg.add(doctorButton);
-
-
         JPanel panel_6 = new JPanel();
-        panel_6.setBackground(Color.MAGENTA);
+        panel_6.setBackground(new Color(30, 144, 255));
         centerPanel.add(panel_6);
         panel_6.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
         JLabel lblNewLabel_2 = new JLabel("Name");
+        lblNewLabel_2.setFont(new Font("Century", Font.PLAIN, 15));
         panel_6.add(lblNewLabel_2);
 
         nameTextField = new JTextField();
@@ -93,11 +92,12 @@ public class AddWorkerMainPanel extends JPanel {
         nameTextField.setColumns(10);
 
         JPanel panel_7 = new JPanel();
-        panel_7.setBackground(Color.MAGENTA);
+        panel_7.setBackground(new Color(30, 144, 255));
         centerPanel.add(panel_7);
         panel_7.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
         JLabel lblNewLabel_3 = new JLabel("Surname");
+        lblNewLabel_3.setFont(new Font("Century", Font.PLAIN, 15));
         panel_7.add(lblNewLabel_3);
 
         surnameTextField = new JTextField();
@@ -105,39 +105,65 @@ public class AddWorkerMainPanel extends JPanel {
         surnameTextField.setColumns(10);
 
         JPanel panel_8 = new JPanel();
-        panel_8.setBackground(Color.MAGENTA);
+        panel_8.setBackground(new Color(30, 144, 255));
         centerPanel.add(panel_8);
         panel_8.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
         JLabel lblNewLabel_3_1 = new JLabel("Mail Address");
+        lblNewLabel_3_1.setFont(new Font("Century", Font.PLAIN, 15));
         panel_8.add(lblNewLabel_3_1);
 
         textField_2 = new JTextField();
         textField_2.setColumns(10);
         panel_8.add(textField_2);
 
+        JPanel panel_15 = new JPanel();
+        panel_15.setBackground(new Color(30, 144, 255));
+        centerPanel.add(panel_15);
+        panel_15.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+
+        JLabel genderLabel = new JLabel("Gender");
+        genderLabel.setFont(new Font("Century", Font.PLAIN, 15));
+        panel_15.add(genderLabel);
+
+        JRadioButton rdbtnNewRadioButton = new JRadioButton("Male");
+        genderButtonGroup.add(rdbtnNewRadioButton);
+        panel_15.add(rdbtnNewRadioButton);
+
+        JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Female");
+        genderButtonGroup.add(rdbtnNewRadioButton_1);
+        panel_15.add(rdbtnNewRadioButton_1);
+
+        JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Unknown");
+        genderButtonGroup.add(rdbtnNewRadioButton_2);
+        panel_15.add(rdbtnNewRadioButton_2);
+
+
         JPanel panel_9 = new JPanel();
-        panel_9.setBackground(Color.MAGENTA);
+        panel_9.setBackground(new Color(30, 144, 255));
         centerPanel.add(panel_9);
         panel_9.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
         JLabel lblNewLabel_3_2 = new JLabel("Medical Speciality");
+        lblNewLabel_3_2.setFont(new Font("Century", Font.PLAIN, 15));
         panel_9.add(lblNewLabel_3_2);
 
         String[] words = new String[]{"Cardiology", "Neurology", "Immunology", "Dermatology", "Radiology", "Pathology",
                 "Pediatrics"};
-        comboBox = new JComboBox(words);
+        JComboBox comboBox = new JComboBox( words  );
+        comboBox.setFont(new Font("Century", Font.PLAIN, 15));
         //ACTION LISTENER
         comboBox.setMaximumRowCount(20);
         panel_9.add(comboBox);
 
         Panel panel_10 = new Panel();
-        panel_10.setBackground(Color.MAGENTA);
+        panel_10.setBackground(new Color(30, 144, 255));
         centerPanel.add(panel_10);
         panel_10.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
-
         JButton btnNewButton = new JButton("FINISH");
+        btnNewButton.setFont(new Font("Century", Font.PLAIN, 15));
+
         ActionListener listener = new AddWorkerController(admin, this);
         btnNewButton.addActionListener(listener);
         panel_10.add(btnNewButton);
