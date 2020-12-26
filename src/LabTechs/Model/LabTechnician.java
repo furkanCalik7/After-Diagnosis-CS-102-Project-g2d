8cbe6fe7d1ed34c43016cca72df23c93441de6ca
Bilgehan Sandıkcı <59236563+Bilgehansandikci@users.noreply.github.com>
2020-12-26 03:44:32 +0300

    Lab tech updated

--- src/LabTechs/Model/LabTechnician.java
+++ src/LabTechs/Model/LabTechnician.java
@@ -1,9 +1,7 @@
 package LabTechs.Model;
 
-
-import Doctor.Model.Doctor;
-import JDBC.Message;
-import JDBC.User;
+import Admin.model.User;
+import JDBC.MySQLAccess;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -11,21 +9,41 @@ import java.util.ArrayList;
 public class LabTechnician extends User {
 
     private ArrayList<Test> tests;
-    private ArrayList<Message> inbox;
-    private ArrayList<Message> outbox;
+    private ArrayList<TestRequest> testRequests;
+    private MySQLAccess mySQLAccess;
 
-    public LabTechnician(java.lang.String username, java.lang.String password, java.lang.String email, java.lang.String name, java.lang.String surname, java.lang.String sex, java.lang.String userType) {
-        super(username, password, email, name, surname, sex, "LabTechnician");
+    public LabTechnician(String username, String password, String email, String name, String surname, String sex) {
+        super(username, "LabTechnician", password, email, name, surname, sex);
+        mySQLAccess = new MySQLAccess();
+        testRequests = mySQLAccess.getTestRequest(getUsername());
+        tests = mySQLAccess.getTestOfLabTech(getUsername());
     }
 
-    //TODO Send test results
+    public LabTechnician(int user_id, String username, String password, String email, String name, String surname, String sex) {
+        super(user_id, username, "LabTechnician", password, email, name, surname, sex);
+        //testRequests = mySQLAccess.getTestRequest(getUsername());
+        //tests = mySQLAccess.getTestOfLabTech(getUsername());
+    }
+
+    public ArrayList<Test> getTests() {
+        return tests;
+    }
+
+    public ArrayList<TestRequest> getTestRequests() {
+        return testRequests;
+    }
+
+    public boolean deleteTestRequest(TestRequest testRequest) {
+        return mySQLAccess.deleteTestRequest(testRequest);
+    }
     public void sendTestResult(Test test) {
         test.sendTest();
     }
 
-    public Test createANewTest(String receiver_username, String test_name, String patient_username, File file) {
+    public Test createNewTest(String receiver_username, String test_name, String patient_username, File file) {
         return Test.newTest(receiver_username, this.getUsername(), test_name, patient_username, file);
     }
+
 }
-//TODO Take test requests
+
 
